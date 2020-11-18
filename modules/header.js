b32e974d90f6fbca1143e3df6025ed7bc32ed99e
Coe <madisonmacf@gmail.com>
2020-11-17 19:41:19 -0500

    cleaned header.js

--- modules/header.js
+++ modules/header.js
@@ -1,21 +1,26 @@
 function buildHeader() {
+    //temporary, need to build database to check if users exist
+    let loggedIn = true;
+    
+    let navHTML = '<nav class="navbar navbar-expand-lg navbar-light bg-light">' +
+                    '<a class="navbar-brand" href="#">Color<b>426</b></a>' +
+                    '<div class="navbar-expand" id="navbarNav">' +
+                        '<ul class="navbar-nav">' +
+                            '<li class="nav-item">' +
+                                '<a class="nav-link" href="index.html">Explore</a>' +
+                            '</li>';
 
-    // if user logged in 
+    // if user logged in
+    if (loggedIn) {
+        // link to profile page
+    }
+    else {}
+    // temporarily available no matter what for testing etc.
+    // link to login page
+    navHTML += '<li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>'
 
-    // if user not logged in
-    $('header').append('<nav class="navbar navbar-expand-lg navbar-light bg-light">' +
-        '<a class="navbar-brand" href="#">Color<b>426</b></a>' +
-        '<div class="navbar-expand" id="navbarNav">' +
-            '<ul class="navbar-nav">' +
-                '<li class="nav-item">' +
-                    '<a class="nav-link" href="index.html">Explore</a>' +
-                '</li>' +
-                '<li class="nav-item">' +
-                    '<a class="nav-link" href="login.html">Login</a>' +
-                '</li>' +
-            '</ul>' +
-        '</div>' +
-    '</nav>');
+    navHTML += '</ul></div></nav>';
+    $('header').append(navHTML);
 
 }
 
