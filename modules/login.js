ea713532bfa605f5ecc3a5e611a43ab827017a1d
Nicholas Georgiou <42646588+ach1lles11@users.noreply.github.com>
2020-11-19 15:20:07 -0500

    Login/Signup Init

--- modules/login.js
+++ modules/login.js
@@ -1,3 +1,25 @@
-async function basicLogin(user, password) {
-    
-}
+emaildocument.getElementById()
+
+// Create an account
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((user) => {
+    // Signed in 
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+
+// Sign into your account
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((user) => {
+    // Signed in 
+    // ...
+})
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+});
