467e698ccaf230d8e8e3503dc23e951cde8edab8
Katelyn Cline <katelyn9@live.unc.edu>
2020-11-23 16:04:34 -0500

    search! kinda

--- modules/headblade.js
+++ modules/headblade.js
@@ -22,6 +22,8 @@ function buildDocHead() {
     $('head').append ('<script type="module" src="../modules/scroll_listener.js"></script>');
     $('head').append ('<script type="module" src="../modules/color_card/color_card.js"></script>');
     $('head').append ('<script type="module" src="../modules/auth.js"></script>');
+    $('head').append ('<script type="module" src="../modules/search.js"></script>');
+
 }
 
 $(document).ready(function () {
